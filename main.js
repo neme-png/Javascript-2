05c5c7329bad1cc533656253452f4a6e7abacb04
Fabio Santangelo <fabbomaui04gmail.com>
2024-11-16 16:45:15 +0100

    Completamento es javascript2

---
+++ main.js
@@ -0,0 +1,15 @@
+let num1 = 1;
+let num2 =2;
+let num3 = 3;
+let num4 = 4;
+let num5 = 5;
+
+let somma = num1 + num2 + num3 + num4 + num5;
+
+console.log(somma)
+
+let media = somma / 5
+
+console.log(media)
+
+console.log("la somma tra i numeri equivale a " + somma + " e la media equivale a "+ media)
